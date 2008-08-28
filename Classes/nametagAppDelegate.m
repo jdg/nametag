e8763a7d8897307710a3de07ac90839230fc39e6
Jonathan George <jonathan@jdg.net>
2008-08-28 17:37:50 -0500

    Add PinchMedia Analytics.

--- Classes/nametagAppDelegate.m
+++ Classes/nametagAppDelegate.m
@@ -9,6 +9,8 @@
 #import "nametagAppDelegate.h"
 #import "nametagViewController.h"
 
+#import "Beacon.h"
+
 @implementation nametagAppDelegate
 
 @synthesize window;
@@ -16,11 +18,17 @@
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {	
+	NSString *applicationCode = @"8a0049bcff7782bb9322a6bacb788a68";
+	[Beacon initAndStartBeaconWithApplicationCode:applicationCode useCoreLocation:YES];
+
 	// Override point for customization after app launch	
     [window addSubview:viewController.view];
 	[window makeKeyAndVisible];
 }
 
+- (void)applicationWillTerminate:(UIApplication *)application {
+	[[Beacon shared] endBeacon];
+}
 
 - (void)dealloc {
     [viewController release];
